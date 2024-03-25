5e9d39db63d7bafe5c73fecf92e1970e774c3fce
Adrienne Stilp <amstilp@uw.edu>
2024-03-25 16:51:38 -0700

    Use outdir instead of outfile in argument parser
    It no longer makes sense to have an outfile argument, because the
    code now outputs multiple files. Now it will write the files to
    a directory with a standard name, instead of trying to create a single
    file with everything in it.

--- query_pgs_by_pmids.py
+++ query_pgs_by_pmids.py
@@ -1,8 +1,11 @@
 from __future__ import print_function
-import pgs_catalog_client
+import os
 import argparse
 import json
+
 import pandas as pd
+
+import pgs_catalog_client
 
 
 def write_to_json(results, filename):
@@ -11,13 +14,27 @@ def write_to_json(results, filename):
     with open(filename, "w") as f:
         f.write(json.dumps([x.to_dict() for x in results], default=str, indent=2))
 
+def get_pgs_records(pmids):
+    pgs_records = []
+    for pmid in pmids:
+        response = score_api.search_scores(pmid=pmid)
+        pgs_records = pgs_records + response.results
+    return pgs_records
+
+def get_metrics_records(pmids):
+    metrics_records = []
+    for pmid in pmids:
+        response = metrics_api.search_performance_metrics(pmid=pmid)
+        metrics_records = metrics_records + response.results
+    return metrics_records
 
 if __name__ == "__main__":
     # Parse arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument("--pmid-file", type=str, help="CSV file with PMIDs in a column")
     parser.add_argument("--pmid-header", type=str, default="PMID", help="Header for PMID column in pmid-file")
-    parser.add_argument("--outfile", help="Output tsv file with pmid, pgp_id, pgs_type, and pgs_id")
+    parser.add_argument("--outdir", help="Directory in which output will be stored", default=".")
+
     args = parser.parse_args()
 
     pubs = pd.read_csv(args.pmid_file)
@@ -38,20 +55,12 @@ if __name__ == "__main__":
 
     # Get PGS records associated with each PMID
     # These are PGS scores that are developed in the publication. It does not include evaluation/metrics.
-    pgs_records = []
-    for pmid in pmids:
-        response = score_api.search_scores(pmid=pmid)
-        pgs_records = pgs_records + response.results
-
-    # Write output to a json file.
-    # Note: you can recreate the score instance with Score(**record.to_dict())
-    write_to_json(pgs_records, "records_pgs.json")
+    pgs_records = get_pgs_records(pmids)
 
     # Get any metrics associated with a publication.
-    metrics_records = []
-    for pmid in pmids:
-        response = metrics_api.search_performance_metrics(pmid=pmid)
-        metrics_records = metrics_records + response.results
+    metrics_records = get_pgs_records(pmids)
 
     # Write output to a json file.
-    write_to_json(metrics_records, "records_metrics.json")
+    os.makedirs(args.outdir, exist_ok=True)
+    write_to_json(metrics_records, os.path.join(args.outdir,"records_metrics.json"))
+    write_to_json(pgs_records, os.path.join(args.outdir, "records_pgs.json"))
